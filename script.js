7aedac17d81ab22cc1512de209900da637867e0a
lightdrk <mohitrawatssj4@gmail.com>
2023-10-03 15:15:50 +0530

    update

--- script.js
+++ script.js
@@ -1,68 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const testimonialsContainer = document.querySelector(".testimonials-container");
-  const prevButton = document.getElementById("prev");
-  const nextButton = document.getElementById("next");
-  const dots = document.querySelectorAll(".dot");
-
-  let currentIndex = 0;
-  let startX = 0;
-  let isDragging = false;
-  const testimonialWidth = testimonialsContainer.querySelector(".testimonial").offsetWidth;
-
-  function updateSliderIndicator() {
+    const testimonialsContainer = document.querySelector(".testimonials-container");
+    const dots = document.querySelectorAll(".dot");
+    
+    let touchStartX = 0;
+    let touchEndX = 0;
+    let currentIndex = 0;
+    const testimonialWidth = testimonialsContainer.querySelector(".testimonial").offsetWidth;
+    
+    function updateSliderIndicator() {
+        dots.forEach((dot, index) => {
+            if (index === currentIndex) {
+                dot.classList.add("active-dot");
+            } else {
+                dot.classList.remove("active-dot");
+            }
+        });
+    }
+    
+    function slideTestimonials(direction) {
+        currentIndex += direction;
+        currentIndex = Math.min(Math.max(currentIndex, 0), testimonialsContainer.childElementCount - 1); // Ensure index stays within bounds
+        testimonialsContainer.scrollLeft = currentIndex * testimonialWidth;
+        updateSliderIndicator();
+    }
+    
+    testimonialsContainer.addEventListener("touchstart", (e) => {
+        touchStartX = e.touches[0].clientX;
+    });
+    
+    testimonialsContainer.addEventListener("touchend", (e) => {
+        touchEndX = e.changedTouches[0].clientX;
+        const swipeDistance = touchStartX - touchEndX;
+        const minSwipeDistance = 50; // Adjust this value to control the sensitivity
+        
+        if (swipeDistance > minSwipeDistance) {
+            // Swiped to the right (move to the next testimonial)
+            slideTestimonials(1);
+        } else if (swipeDistance < -minSwipeDistance) {
+            // Swiped to the left (move to the previous testimonial)
+            slideTestimonials(-1);
+        }
+    });
+    
+    // Event listeners for dot indicators
     dots.forEach((dot, index) => {
-      if (index === currentIndex) {
-        dot.classList.add("active-dot");
-      } else {
-        dot.classList.remove("active-dot");
-      }
+        dot.addEventListener("click", () => {
+            currentIndex = index;
+            testimonialsContainer.scrollLeft = currentIndex * testimonialWidth;
+            updateSliderIndicator();
+        });
     });
-  }
-
-  function slideTestimonials(direction) {
-    currentIndex += direction;
-    currentIndex = Math.min(Math.max(currentIndex, 0), testimonialsContainer.childElementCount - 1); // Ensure index stays within bounds
-    testimonialsContainer.scrollLeft = currentIndex * testimonialWidth;
+    
+    // Initial slider indicator update
     updateSliderIndicator();
-  }
-
-  // Event listeners for slide buttons
-  prevButton.addEventListener("click", () => slideTestimonials(-1));
-  nextButton.addEventListener("click", () => slideTestimonials(1));
-
-  // Event listeners for dot indicators
-  dots.forEach((dot, index) => {
-    dot.addEventListener("click", () => {
-      currentIndex = index * 1; // Multiply by 1 for one testimonial per slide
-      testimonialsContainer.scrollLeft = currentIndex * testimonialWidth;
-      updateSliderIndicator();
-    });
-  });
-
-  // Touch event handling
-  testimonialsContainer.addEventListener("touchstart", (e) => {
-    startX = e.touches[0].clientX;
-    isDragging = true;
-  });
-
-  testimonialsContainer.addEventListener("touchmove", (e) => {
-    if (!isDragging) return;
-    const currentX = e.touches[0].clientX;
-    const diffX = startX - currentX;
-    currentIndex = Math.min(Math.max(currentIndex + (diffX > 0 ? 1 : -1), 0), testimonialsContainer.childElementCount - 1);
-    testimonialsContainer.scrollLeft = currentIndex * testimonialWidth;
-    startX = currentX;
-    updateSliderIndicator();
-    e.preventDefault();
-  });
-
-  testimonialsContainer.addEventListener("touchend", () => {
-    isDragging = false;
-  });
-
-  // Initial slider indicator update
-  updateSliderIndicator();
 });
+
 
 window.addEventListener("scroll", function () {
             const cloud = document.querySelector(".cloud");
